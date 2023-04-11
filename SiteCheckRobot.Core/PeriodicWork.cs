6ffc5eabaf3aecc53ddf25af1d25817d2a46b404
Matthias Jost <8178852+matthiasjost@users.noreply.github.com>
2023-04-11 20:18:15 +0200

    Twilio SMS, extract GetLastSiteHeatlhItem

--- SiteCheckRobot.Core/PeriodicWork.cs
+++ SiteCheckRobot.Core/PeriodicWork.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 using Microsoft.Azure.Cosmos;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+using Twilio.Types;
 
 namespace SiteCheckRobot.Core
 {
@@ -34,10 +37,7 @@ namespace SiteCheckRobot.Core
             Database db = client.GetDatabase(_configuration["CosmosDb"]);
             Container ct = db.GetContainer(_configuration["CosmosContainer"]);
 
-            var query = $"SELECT TOP 1 * FROM c ORDER BY c._ts DESC";
-            var iterator = ct.GetItemQueryIterator<SiteHealthItem>(new QueryDefinition(query));
-            FeedResponse<SiteHealthItem>? resultItems = await iterator.ReadNextAsync();
-            SiteHealthItem? latestItem = resultItems?.FirstOrDefault();
+            SiteHealthItem latestItem = await GetLastSiteHealthItem(ct);
 
             SiteHealthItem siteHealthItem = new SiteHealthItem();
             siteHealthItem.ResponseTimeMs = siteCheck.ResponseTimeMs;
@@ -45,17 +45,38 @@ namespace SiteCheckRobot.Core
 
             if (latestItem != null && latestItem.State != siteHealthItem.State)
             {
+
+                TwilioClient.Init(_configuration["TwilioSid"], _configuration["TwilioAuthToken"]);
+
+                var messageOptions = new CreateMessageOptions(new PhoneNumber(_configuration["TwilioTo"]));
+                messageOptions.From = new PhoneNumber(_configuration["TwilioFrom"]);
+
                 if (latestItem.State == "good" && siteHealthItem.State == "bad")
                 {
                     // state changed from good to bad, do something
+
+                    messageOptions.Body = "Good -> Bad";
+
                 }
                 else if (latestItem.State == "bad" && siteHealthItem.State == "good")
                 {
                     // state changed from bad to good, do something else
+                    messageOptions.Body = "Bad -> Good";
                 }
+
+                var message = await MessageResource.CreateAsync(messageOptions);
             }
 
             await ct.CreateItemAsync(siteHealthItem);
+        }
+
+        private static async Task<SiteHealthItem?> GetLastSiteHealthItem(Container ct)
+        {
+            var query = $"SELECT TOP 1 * FROM c ORDER BY c._ts DESC";
+            var iterator = ct.GetItemQueryIterator<SiteHealthItem>(new QueryDefinition(query));
+            FeedResponse<SiteHealthItem>? resultItems = await iterator.ReadNextAsync();
+            SiteHealthItem? latestItem = resultItems?.FirstOrDefault();
+            return latestItem;
         }
     }
 }
